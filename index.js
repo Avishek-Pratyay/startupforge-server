47352a09622e3038c32605e2e87448b2e8694395
Avishek-Pratyay <avishekp.aiub@gmail.com>
2026-06-25 19:13:13 +0600

    a

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:5173",
-    process.env.CLIENT_URL],
+     "https://startupforge-client-six.vercel.app",],
     credentials: true,
   })
 );
